5807f2ec53736845ec7d80a7c94ce08e559224af
chenhao <chenhao980510@163.com>
2021-10-20 21:33:39 +0800

    add SocketOps InetAddress Socket

--- net/src/Socket.cc
+++ net/src/Socket.cc
@@ -3,45 +3,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#include <arpa/inet.h>
 
 #include "../../base/src/Types.h"
 
 namespace tinyWeb {
 namespace net {
-
-bool Socket::getTcpInfo(struct tcp_info *tcpInfo) const {
-  socklen_t len = sizeof(*tcpInfo);
-  memset(tcpInfo, 0, len);
-  return ::getsockopt(fd_, IPPROTO_TCP, \
-                      TCP_INFO, tcpInfo, &len) == 0;
-}
-
-
-bool Socket::getTcpInfo(char *buf, size_t len) const {
-  struct tcp_info tcpi;
-  bool ok = getTcpInfo(&tcpi);
-  if (ok) {
-    snprintf(buf, len, "unrecovered=%u "
-          "rto=%u ato=%u snd_mss=%u rcv_mss=%u "
-          "lost=%u retrans=%u rtt=%u rttvar=%u "
-          "sshthresh=%u cwnd=%u total_retrans=%u",
-          tcpi.tcpi_retransmits,  // Number of unrecovered [RTO] timeouts
-          tcpi.tcpi_rto,          // Retransmit timeout in usec
-          tcpi.tcpi_ato,          // Predicted tick of soft clock in usec
-          tcpi.tcpi_snd_mss,
-          tcpi.tcpi_rcv_mss,
-          tcpi.tcpi_lost,         // Lost packets
-          tcpi.tcpi_retrans,      // Retransmitted packets out
-          tcpi.tcpi_rtt,          // Smoothed round trip time in usec
-          tcpi.tcpi_rttvar,       // Medium deviation
-          tcpi.tcpi_snd_ssthresh,
-          tcpi.tcpi_snd_cwnd,
-          tcpi.tcpi_total_retrans);  // Total retransmits for entire connection
-  }
-
-  return ok;
-}
 
 void Socket::bindAddress(const InetAddress& localAddr) {
   sockets::bindOrDie(fd_, localAddr.getSockAddr());
@@ -69,25 +35,53 @@ void Socket::shutdownWrite() {
 void Socket::setTcpNoDelay(bool on) {
   int optval = on ? 1 : 0;
   MCHECK(::setsockopt(fd_, IPPROTO_TCP, TCP_NODELAY, \
-                      &optval, static_cast<socklen_t>(optval)));
+          &optval, static_cast<socklen_t>(sizeof(optval))));
 }
 
 void Socket::setReuseAddr(bool on) {
   int optval  = on ? 1 : 0;
   MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, \
-                      &optval, static_cast<socklen_t>(optval)));
+          &optval, static_cast<socklen_t>(sizeof(optval))));
 }
 
 void Socket::setReusePort(bool on) {
   int optval = on ? 1 : 0;
   MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_REUSEPORT, \
-                      &optval, static_cast<socklen_t>(optval)));
+          &optval, static_cast<socklen_t>(sizeof(optval))));
 }
 
 void Socket::setKeepAlive(bool on) {
   int optval = on ? 1 : 0;
   MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_KEEPALIVE, \
-                      &optval, static_cast<socklen_t>(optval)));
+          &optval, static_cast<socklen_t>(sizeof(optval))));
+}
+
+void Socket::setLinger(bool on, int seconds) {
+  struct linger lg;
+  lg.l_onoff = on;
+  if (on) lg.l_linger = seconds;
+  MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_LINGER, \
+                &lg, static_cast<socklen_t>(sizeof(lg))));
+}
+
+void Socket::setReceiveBufferSize(int size) {
+  MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_RCVBUF, \
+              &size, static_cast<socklen_t>(sizeof(size))));
+}
+
+void Socket::setSendBufferSize(int size) {
+  MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_SNDBUF, \
+              &size, static_cast<socklen_t>(sizeof(size))));
+}
+
+void Socket::setReceiveLowat(int size) {
+  MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_RCVLOWAT, \
+              &size, static_cast<socklen_t>(sizeof(size))));
+}
+
+void Socket::setSendLowat(int size) {
+  MCHECK(::setsockopt(fd_, SOL_SOCKET, SO_SNDLOWAT, \
+              &size, static_cast<socklen_t>(sizeof(size))));
 }
 
 }  // namespace net
--- net/src/Socket.h
+++ net/src/Socket.h
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "InetAddress.h"
+#include "../../base/src/noncopyable.hpp"
 
 namespace tinyWeb {
 namespace net {
@@ -20,9 +21,6 @@ class Socket {
 
   int fd() const { return fd_; }
 
-  bool getTcpInfo(struct tcp_info *tcpInfo) const;
-  bool getTcpInfo(char *buf, size_t len) const;
-
   void bindAddress(const InetAddress& localAddr);
   void listen();
   int accept(InetAddress* peerAddr);
@@ -30,12 +28,23 @@ class Socket {
   void shutdownWrite();
 
   void setTcpNoDelay(bool on);
-
+  // 须在使用bindAddress前调用
   void setReuseAddr(bool on);
-
+  // 须在使用bindAddress前调用
   void setReusePort(bool on);
 
   void setKeepAlive(bool on);
+  // 设置close的行为
+  void setLinger(bool on, int seconds);
+  // 设置接受缓冲区大小
+  // 须在connect或listen之前调用
+  void setReceiveBufferSize(int size);
+  // 设置发送缓冲区大小
+  void setSendBufferSize(int size);
+  // 设置接受低水位大小
+  void setReceiveLowat(int size);
+  // 设置发送低水位标志
+  void setSendLowat(int size);
 
  private:
   const int fd_;
